12f79e5a0b669ae70d6d82a3d588e9df2dffd046
Jienan Zhang <zjn0505@hotmail.com>
2018-01-04 14:44:27 +0800

    Add cache

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ mongoose.connect('mongodb://localhost/memo_db');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.url.indexOf("/icon/") === 5 || req.url.indexOf("/css/") === 5 || req.url.indexOf("/script/") === 5) {
+    res.setHeader("Cache-Control", "public, max-age=2592000");
+    res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
+  }
+  if (req.url.indexOf("/script/main.js") === 5) {
+  	res.setHeader("Cache-Control", "public, max-age=3600");
+    res.setHeader("Expires", new Date(Date.now() + 360000).toUTCString());
+  }
   next();
 });
 
