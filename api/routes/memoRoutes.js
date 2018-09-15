5172c9823f2f8a6a4a47b475381aa3553b4a52d1
Jienan Zhang <zjn0505@hotmail.com>
2018-09-15 11:36:08 +0000

    Change route priority and add regex

--- api/routes/memoRoutes.js
+++ api/routes/memoRoutes.js
@@ -2,11 +2,11 @@
 module.exports = function(app) {
 	var memoReqs = require('../controllers/memoController');
 
-	// memo Routes
+	// memo Routes	
+	app.route('^/memo/:memoId([0-9A-Za-z]{4,5}-?$)')
+	.get(memoReqs.read_a_memo)
+
 	app.route('/memo')
 	.post(memoReqs.create_a_memo)
-	.get(memoReqs.read_a_memo)
-	
-	app.route('/memo/:memoId')
 	.get(memoReqs.read_a_memo)
 };
